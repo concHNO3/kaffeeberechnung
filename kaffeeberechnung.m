0d635835eecd99d32ef150c81d72b1e00ef17f12
David Z <developer.Pni0@gmail.com>
2012-04-04 12:29:05 +0200

    Tag22

--- kaffeeberechnung.m
+++ kaffeeberechnung.m
@@ -3,36 +3,29 @@
 
 % Werte Hier eintragen:
 Kaffegross =  1;
-Kaffeklein = 19;
+Kaffeklein = 21;
 
 Preisgross = 0.80;
 Preisklein = 0.50;
 
-Zeitraum   = 21;
+Zeitraum   = 22;
 ArbeitstageProWoche = 4;
-
 
 %Berechnung:
 Gesamtpreis = (Kaffeklein * Preisklein + Kaffegross * Preisgross);
-
 GesamtKaffee = (Kaffegross + Kaffeklein);
-
 DurchschnittlicherKaffeePreis = (Kaffeklein * Preisklein + Kaffegross * Preisgross)/(Kaffegross + Kaffeklein);
-
 DurchschnittlicherKaffeeProTag = (Kaffegross + Kaffeklein)/Zeitraum;
-
 DurchschnittlicherKaffeeProArbeitstag = (Kaffegross + Kaffeklein) / (Zeitraum * ArbeitstageProWoche/7);
-
 DurchschnittlicherPreisProTag = (Kaffeklein * Preisklein + Kaffegross * Preisgross)/Zeitraum;
-
 DurchschnittlicherPreisProArbeitstag= (Kaffeklein * Preisklein + Kaffegross * Preisgross)/ (Zeitraum * ArbeitstageProWoche/7);
 
-printf("im Gesamten Zeitraum getrunkene Kaffee: %d \n", Gesamtpreis);
+printf("im Gesamten Zeitraum getrunkene Kaffee: %d \n", GesamtKaffee);
 printf("Durchschnittliche Anzahl der Kaffee pro Tag: %f \n", DurchschnittlicherKaffeeProTag);
 printf("Durchschnittliche Anzahl der Kaffee pro Arbeitstag: %f \n", DurchschnittlicherKaffeeProArbeitstag);
-printf("Gesamtpreis der im Zeitraum getrunkenen Kaffee: %f \n", Gesamtpreis);
-printf("Durchschnittlicher Preis pro Kaffee: %f \n", DurchschnittlicherKaffeePreis);
-printf("Durchschnittlicher Preis pro Tag: %f \n", DurchschnittlicherPreisProTag);
-printf("Durchschnittlicher Preis pro Arbeitstag: %f \n", DurchschnittlicherPreisProArbeitstag);
+printf("Gesamtpreis der im Zeitraum getrunkenen Kaffee: %f Euro\n", Gesamtpreis);
+printf("Durchschnittlicher Preis pro Kaffee: %f Euro\n", DurchschnittlicherKaffeePreis);
+printf("Durchschnittlicher Preis pro Tag: %f Euro\n", DurchschnittlicherPreisProTag);
+printf("Durchschnittlicher Preis pro Arbeitstag: %f Euro\n", DurchschnittlicherPreisProArbeitstag);
 
 
